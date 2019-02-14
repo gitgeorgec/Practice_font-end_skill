d8a6f9fdfcecdba866f57283f941533ae29b4ff1
george <ccchang0661@gmail.com>
2019-02-14 16:12:36 +0800

    reduxstrgam from wesbos

---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import Photo from './Photo'
+import Comments from './Commemts'
+
+class Single extends Component{
+    render(){
+        const { postId } = this.props.params
+        const i = this.props.posts.findIndex((post)=> post.code === postId)
+        const post = this.props.posts[i]
+        const postComments = this.props.comments[postId] || []
+
+        return(
+            <div className="single-photo">
+                <Photo {...this.props} i={i} post={post} />
+                <Comments postComments={postComments} {...this.props}/>
+            </div>
+        )
+    }
+}
+
+export default Single
+
---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,34 @@
+// reducer takes two things
+//1. the action 
+//2. copy of current state
+function postComments(state=[], action){
+    switch(action.type){
+        case "ADD_COMMENT":
+            return [...state,{
+                user:action.author,
+                text:action.comment
+            }]   
+        case "REMOVE_COMMENT":
+        console.log("remove")
+            return [
+                ...state.slice(0,action.i),
+                ...state.slice(action.i+1)
+            ]
+        default:
+            return state
+    }
+}
+
+
+function comments(state=[], action){
+    console.log(action.postId)
+    if(typeof action.postId !== 'undefined'){
+        return {
+            ...state,
+            [action.postId]:postComments(state[action.postId], action)
+        }
+    }
+    return state
+}
+
+export default comments
